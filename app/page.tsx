2ec2b3e77565adc5440b623e5f25279c205456bd
Afrazsajid <101873923+Afrazsajid@users.noreply.github.com>
2025-02-27 03:09:27 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import FeaturesSection from "@/components/FeaturesSection";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorksSection";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
